9ae8b62f96d587fd02fffce212aed09a065b7dfc
cesaraugusto316 <cesaraugusto316@protonmail.com>
2023-09-17 15:48:06 -0500

    ✨ updates

--- app/bots/chatGPTBot.js
+++ app/bots/chatGPTBot.js
@@ -4,7 +4,7 @@ const { CoreClass, EVENTS } = require("@bot-whatsapp/bot");
 class ChatGPTBot extends CoreClass {
 
   queue = [];
-  openAI;
+  openAI = undefined;
 
 
   constructor(_dataBase, _provider) {
@@ -17,56 +17,46 @@ class ChatGPTBot extends CoreClass {
     const { ChatGPTAPI } = await import('chatgpt');
 
     this.openAI = new ChatGPTAPI({
-      apiKey: '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'
+      apiKey: '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'
     });
   }
 
 
   handleMsg = async (ctx) => {
     if (!ctx.body) return
-
-    // let prevMsg = await this.databaseClass.getPrevByNumber(ctx.from);
-    // const refToContinue = this.flowClass.findBySerialize(prevMsg?.refSerialize);
-
-    // if (prevMsg?.ref) {
-    //   delete prevMsg._id;
-    //   const ctxByNumber = toCtx({
-    //     body,
-    //     from,
-    //     prevRef: prevMsg.refSerialize,
-    //   });
-    //   await this.databaseClass.save(ctxByNumber);
-    // }
-
-    const interaccionChatGPT = (
-      await
-        this.openAI
-          .sendMessage(
-            ctx.body,
-            {
-              conversationId: (
-                !this.queue.length ?
-                  undefined :
-                  this.queue[this.queue.length - 1].conversationId
-              ),
-              parentMessageId: (
-                !this.queue.length ?
-                  undefined :
-                  this.queue[this.queue.length - 1].id
-              ),
-            }
-          )
-    );
-    this.queue.push(interaccionChatGPT);
-    const parseMessage = {
-      ...interaccionChatGPT,
-      answer: interaccionChatGPT.text
+    try {
+      const interaccionChatGPT = (
+        await
+          this.openAI
+            .sendMessage(
+              ctx.body,
+              {
+                conversationId: (
+                  !this.queue.length ?
+                    undefined :
+                    this.queue[this.queue.length - 1].conversationId
+                ),
+                parentMessageId: (
+                  !this.queue.length ?
+                    undefined :
+                    this.queue[this.queue.length - 1].id
+                ),
+              }
+            )
+      );
+      this.queue.push(interaccionChatGPT);
+      const parseMessage = {
+        ...interaccionChatGPT,
+        answer: interaccionChatGPT.text
+      }
+      // sends the chatGPT response to the user via whatsapp
+      this.sendFlowSimple([parseMessage], ctx.from)
+      return interaccionChatGPT;
     }
-
-    // sends the chatGPT response to the user via whatsapp
-    this.sendFlowSimple([parseMessage], ctx.from)
-
-    return interaccionChatGPT;
+    catch (error) {
+      console.log(error)
+      return
+    }
   }
 }
 
